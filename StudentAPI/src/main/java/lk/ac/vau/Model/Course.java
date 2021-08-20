3c682a89f810240efe7aaca1b5c282f152ad9f4f
Chathudya Jayawardana <chathudya@gmail.com>
2021-08-20 09:48:31 +0530

    commit

--- StudentAPI/src/main/java/lk/ac/vau/Model/Course.java
+++ StudentAPI/src/main/java/lk/ac/vau/Model/Course.java
@@ -1,5 +1,24 @@
 package lk.ac.vau.Model;
 
 public class Course {
-
+	public String courseid;
+	public String coursename;
+	public Course(String courseid, String coursename) {
+		super();
+		this.courseid = courseid;
+		this.coursename = coursename;
+	}
+	public String getCourseid() {
+		return courseid;
+	}
+	public void setCourseid(String courseid) {
+		this.courseid = courseid;
+	}
+	public String getCoursename() {
+		return coursename;
+	}
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+	
 }
--- StudentAPI/src/main/java/lk/ac/vau/Model/Lecturer.java
+++ StudentAPI/src/main/java/lk/ac/vau/Model/Lecturer.java
@@ -1,5 +1,33 @@
 package lk.ac.vau.Model;
 
 public class Lecturer {
-
+	public String lecturerid;
+	public String name;
+	public String courseid;
+	public Lecturer(String lecturerid, String name, String courseid) {
+		super();
+		this.lecturerid = lecturerid;
+		this.name = name;
+		this.courseid = courseid;
+	}
+	public String getLecturerid() {
+		return lecturerid;
+	}
+	public void setLecturerid(String lecturerid) {
+		this.lecturerid = lecturerid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourseid() {
+		return courseid;
+	}
+	public void setCourseid(String courseid) {
+		this.courseid = courseid;
+	}
+	
+	
 }
